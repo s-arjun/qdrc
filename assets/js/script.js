d208ab8ecfd80385a20ec4d628869f7c7ec99136
Arjun S <arjunparayelil@outlook.com>
2022-03-05 16:06:04 +0530

    Changed all

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,32 +10,6 @@ $( document ).ready(function() {
 })
 
 
-$( document ).ready(function() {
-
-     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 5000,
-        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
-
-
-});
-
 $(document).ready(function(){
 
     $(".filter-button").click(function(){
